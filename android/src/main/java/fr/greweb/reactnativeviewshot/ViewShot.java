eff80b97363f10b8a430b6372e534875db5bf1de
Hung Huynh <huynhphihung1990@gmail.com>
2018-11-12 21:45:16 +0700

    update

--- android/src/main/java/fr/greweb/reactnativeviewshot/ViewShot.java
+++ android/src/main/java/fr/greweb/reactnativeviewshot/ViewShot.java
@@ -17,17 +17,26 @@ import android.opengl.GLES20;
 import android.widget.ScrollView;
 import android.util.Log;
 
+import org.webrtc.EglRenderer;
+import org.webrtc.RendererCommon.ScalingType;
+import com.oney.WebRTCModule.WebRTCView;
+import com.oney.WebRTCModule.SurfaceViewRenderer;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.uimanager.NativeViewHierarchyManager;
 import com.facebook.react.uimanager.UIBlock;
 
+import android.os.Environment;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
-import  java.nio.IntBuffer;
+import java.nio.IntBuffer;
 
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLContext;
@@ -39,7 +48,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class ViewShot implements UIBlock {
     static final String ERROR_UNABLE_TO_SNAPSHOT = "E_UNABLE_TO_SNAPSHOT";
-    
+
     private int tag;
     private String extension;
     private Bitmap.CompressFormat format;
@@ -50,18 +59,18 @@ public class ViewShot implements UIBlock {
     private String result;
     private Promise promise;
     private Boolean snapshotContentContainer;
-    
+
     public ViewShot(
-                    int tag,
-                    String extension,
-                    Bitmap.CompressFormat format,
-                    double quality,
-                    @Nullable Integer width,
-                    @Nullable Integer height,
-                    File output,
-                    String result,
-                    Boolean snapshotContentContainer,
-                    Promise promise) {
+            int tag,
+            String extension,
+            Bitmap.CompressFormat format,
+            double quality,
+            @Nullable Integer width,
+            @Nullable Integer height,
+            File output,
+            String result,
+            Boolean snapshotContentContainer,
+            Promise promise) {
         this.tag = tag;
         this.extension = extension;
         this.format = format;
@@ -73,49 +82,39 @@ public class ViewShot implements UIBlock {
         this.snapshotContentContainer = snapshotContentContainer;
         this.promise = promise;
     }
-    
+
+    public String BitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp=Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+
     @Override
     public void execute(NativeViewHierarchyManager nativeViewHierarchyManager) {
         OutputStream os = null;
         View view = nativeViewHierarchyManager.resolveView(tag);
         if (view == null) {
-            promise.reject(ERROR_UNABLE_TO_SNAPSHOT, "No view found with reactTag: "+tag);
+            promise.reject(ERROR_UNABLE_TO_SNAPSHOT, "No view found with reactTag: " + tag);
             return;
         }
         try {
+
             // Hard code solution for webrtc RTCView
-            if (view.getClass().getName().equals("com.oney.WebRTCModule.WebRTCView")) {
-                final SurfaceView sw = (SurfaceView) ((ViewGroup) view).getChildAt(0);
-                
-                try {
-                    final Object capturedSignal = Class.forName("com.oney.WebRTCModule.SurfaceViewRenderer")
-                    .getMethod("getCapturedSignal")
-                    .invoke(sw);
-                    
-                    Class.forName("com.oney.WebRTCModule.SurfaceViewRenderer")
-                    .getMethod("createBitmapFromFrame")
-                    .invoke(sw);
-                    
-                    Log.d("RNViewShotHack", "Waiting for captured done");
-                    synchronized (capturedSignal) {
-                        capturedSignal.wait();
-                        Log.d("RNViewShotHack", "Captured done waiting");
-                    }
-                    
-                    Bitmap capturedBitmap = (Bitmap) Class.forName("com.oney.WebRTCModule.SurfaceViewRenderer")
-                    .getMethod("getCapturedBitmap")
-                    .invoke(sw);
-                    
-                    os = new ByteArrayOutputStream();
-                    capturedBitmap.compress(format, (int)(100.0 * quality), os);
-                    byte[] bytes = ((ByteArrayOutputStream) os).toByteArray();
-                    final String data = "data:image/"+extension+";base64," + Base64.encodeToString(bytes, Base64.NO_WRAP);
-                    promise.resolve(data);
-                } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e){
-                    promise.reject(ERROR_UNABLE_TO_SNAPSHOT, "Class not found runtime error");
-                }
-            }
-            else {
+            String nameClass = view.getClass().getName();
+
+            if (nameClass.equals("com.oney.WebRTCModule.WebRTCView")) {
+                final WebRTCView webrtcView = (WebRTCView) view;
+                final SurfaceViewRenderer sw = (SurfaceViewRenderer) webrtcView.getSurfaceView();
+
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+                File file = new File(Environment.getExternalStorageDirectory() + File.separator + "DCIM/" + timeStamp + ".png");
+
+                sw.setPromise(this.promise);
+                sw.saveFrame(this.extension, this.output);
+
+            } else {
                 if ("file".equals(result)) {
                     os = new FileOutputStream(output);
                     captureView(view, os);
@@ -138,12 +137,11 @@ public class ViewShot implements UIBlock {
                     promise.reject(ERROR_UNABLE_TO_SNAPSHOT, "Unsupported result: " + result + ". Try one of: file | base64 | data-uri");
                 }
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             promise.reject(ERROR_UNABLE_TO_SNAPSHOT, "Failed to capture view snapshot");
-        }
-        finally {
+
+        } finally {
             if (os != null) {
                 try {
                     os.close();
@@ -153,24 +151,25 @@ public class ViewShot implements UIBlock {
             }
         }
     }
-    
+
     /**
      * Screenshot a view and return the captured bitmap.
+     *
      * @param view the view to capture
      * @return the screenshot or null if it failed.
      */
-    private void captureView (View view, OutputStream os) {
+    private void captureView(View view, OutputStream os) {
         int w = view.getWidth();
         int h = view.getHeight();
-        
+
         if (w <= 0 || h <= 0) {
             throw new RuntimeException("Impossible to snapshot the view: view is invalid");
         }
-        
+
         //evaluate real height
         if (snapshotContentContainer) {
-            h=0;
-            ScrollView scrollView = (ScrollView)view;
+            h = 0;
+            ScrollView scrollView = (ScrollView) view;
             for (int i = 0; i < scrollView.getChildCount(); i++) {
                 h += scrollView.getChildAt(i).getHeight();
             }
@@ -178,13 +177,13 @@ public class ViewShot implements UIBlock {
         Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(bitmap);
         view.draw(c);
-        
+
         if (width != null && height != null && (width != w || height != h)) {
             bitmap = Bitmap.createScaledBitmap(bitmap, width, height, true);
         }
         if (bitmap == null) {
             throw new RuntimeException("Impossible to snapshot the view");
         }
-        bitmap.compress(format, (int)(100.0 * quality), os);
+        bitmap.compress(format, (int) (100.0 * quality), os);
     }
 }
