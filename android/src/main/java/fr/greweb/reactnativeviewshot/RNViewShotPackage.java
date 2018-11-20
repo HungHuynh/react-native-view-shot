47a529852cafdda4c4b36971765aac01770ca30b
Hung Huynh <huynhphihung1990@gmail.com>
2018-11-21 01:28:16 +0700

    fix build

--- android/src/main/java/fr/greweb/reactnativeviewshot/RNViewShotPackage.java
+++ android/src/main/java/fr/greweb/reactnativeviewshot/RNViewShotPackage.java
@@ -16,7 +16,7 @@ public class RNViewShotPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNViewShotModule(reactContext));
     }
 
-    @Override
+    //@Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
